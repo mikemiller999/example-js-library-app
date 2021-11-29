d85ed0bb0957565b4d4974c65e84bde4c1b69082
Mike Miller <mikemille999@gmail.com>
2021-11-29 03:02:31 -0600

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "antd/dist/antd.css";
+import { Button, DatePicker } from "antd";
 
+const App = () => (
+  <>
+    <Button type="primary">PRESS ME</Button>
+    <DatePicker placeholder="select date" />
+  </>
+);
 Vue.config.productionTip = false;
-
+// I really dont understand how to implement this
 new Vue({
   router,
   render: (h) => h(App),
